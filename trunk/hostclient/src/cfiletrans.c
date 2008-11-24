6235a658c9bf18be1773900ade032ae67a553864
lizeliang.linux <lizeliang.linux@18169ff8-9de4-11dd-b47d-f93aca0b5c28>
2008-11-24 12:44:15 +0000

    添加了cfiletrans.c、filetrans.h、ifiletrans.c 修改了hctthread.c、hccthread.c

---
+++ trunk/hostclient/src/cfiletrans.c
@@ -0,0 +1,10 @@
+#include <gtk/gtk.h>
+#include "filetrans.h"
+
+extern void cb_fview_upload_activate(GtkMenuItem *menuitem, gpointer user_data);
+
+extern void cb_fview_downolad_activate(GtkMenuItem *menuitem, gpointer user_data);
+
+extern void cb_fview_rename_activate(GtkMenuItem *menuitem, gpointer user_data);
+
+extern void cb_fview_delete_activate(GtkMenuItem *menuitem, gpointer user_data);
---
+++ trunk/hostclient/src/ifiletrans.c
@@ -0,0 +1,134 @@
+#ifdef HAVE_CONFIG_H
+#include <config.h>
+#endif
+
+#include <sys/types.h>
+#include <sys/stat.h>
+#include <unistd.h>
+#include <string.h>
+#include <stdio.h>
+
+#include <gdk/gdkkeysyms.h>
+#include <gtk/gtk.h>
+
+#include "filetrans.h"
+#include "mwindow.h"
+#include "support.h"
+
+#define GLADE_HOOKUP_OBJECT(component,widget,name)\
+	g_object_set_data_full(G_OBJECT(component), name, \
+	gtk_widget_ref(widget),(GDestroyNotify)gtk_widget_unref)
+
+#define GLADE_HOOKUP_OBJECT_NO_REF(component,widget,name)\
+	g_object_set_data(G_OBJECT(component), name, widget)
+	
+GtkWidget* create_popup_menu_fview(void)
+{
+	GtkWidget *popup_menu_fview;
+	GtkWidget *popup_separator;
+	GtkWidget *fview_rename;
+	GtkWidget *image203;
+	GtkWidget *fview_delete;
+	GtkWidget *image204;
+	GtkWidget *edit_separator_one;
+	GtkWidget *edit_upload;
+	GtkWidget *image205;
+	GtkWidget *edit_downolad;
+	GtkWidget *image206;
+	GtkTooltips *tooltips;
+
+	tooltips = gtk_tooltips_new();
+
+
+	popup_menu_fview = gtk_menu_new();
+
+	popup_separator = gtk_separator_menu_item_new();
+	gtk_widget_show(popup_separator);
+	gtk_container_add(GTK_CONTAINER(popup_menu_fview), popup_separator);
+	gtk_widget_set_sensitive(popup_separator, FALSE);
+
+	fview_rename = gtk_image_menu_item_new_with_mnemonic(
+		_("\351\207\215\345\221\275\345\220\215\346\226\207\344\273\266"));
+	gtk_widget_show(fview_rename);
+	gtk_container_add(GTK_CONTAINER(popup_menu_fview), fview_rename);
+	gtk_tooltips_set_tip(tooltips, fview_rename,
+				_("\351\207\215\345\221\275\345\220\215\351"
+				"\200\211\344\270\255\347\232\204\346\226"
+				"\207\344\273\266"), NULL);
+
+	image203 = gtk_image_new_from_stock("gtk-refresh", GTK_ICON_SIZE_MENU);
+	gtk_widget_show(image203);
+	gtk_image_menu_item_set_image(GTK_IMAGE_MENU_ITEM(fview_rename), image203);
+
+	fview_delete = gtk_image_menu_item_new_with_mnemonic(
+			_("\345\210\240\351\231\244\346\226\207\344\273\266"));
+	gtk_widget_show(fview_delete);
+	gtk_container_add(GTK_CONTAINER(popup_menu_fview), fview_delete);
+	gtk_tooltips_set_tip(tooltips, fview_delete,
+			_("\345\210\240\351\231\244\351\200\211\344"
+			"\270\255\347\232\204\346\226\207\344\273\266"), NULL);
+
+	image204 = gtk_image_new_from_stock("gtk-delete", GTK_ICON_SIZE_MENU);
+	gtk_widget_show(image204);
+	gtk_image_menu_item_set_image(GTK_IMAGE_MENU_ITEM(fview_delete), image204);
+
+	edit_separator_one = gtk_separator_menu_item_new();
+	gtk_widget_show(edit_separator_one);
+	gtk_container_add(GTK_CONTAINER(popup_menu_fview), edit_separator_one);
+	gtk_widget_set_sensitive(edit_separator_one, FALSE);
+
+	edit_upload = gtk_image_menu_item_new_with_mnemonic(
+			_("\344\270\212\344\274\240\346\226\207\344\273\266"));
+	gtk_widget_show(edit_upload);
+	gtk_container_add(GTK_CONTAINER(popup_menu_fview), edit_upload);
+	gtk_tooltips_set_tip(tooltips, edit_upload,
+			_("\344\273\216arm\347\263\273\347\273\237"
+			"\344\270\212\344\274\240\346\226\207\344"
+			"\273\266\345\210\260\346\234\254\346\234\272"), NULL);
+
+	image205 = gtk_image_new_from_stock("gtk-goto-top", GTK_ICON_SIZE_MENU);
+	gtk_widget_show(image205);
+	gtk_image_menu_item_set_image(GTK_IMAGE_MENU_ITEM(edit_upload), image205);
+
+	edit_downolad = gtk_image_menu_item_new_with_mnemonic(
+			_("\344\270\213\350\275\275\346\226\207\344\273\266"));
+	gtk_widget_show(edit_downolad);
+	gtk_container_add(GTK_CONTAINER(popup_menu_fview), edit_downolad);
+	gtk_tooltips_set_tip(tooltips, edit_downolad,
+			_("\344\273\216\346\234\254\346\234\272\344"
+			"\270\213\350\275\275\346\226\207\344\273"
+			"\266\345\210\260arm\347\263\273\347\273\237\344\270\212"), NULL);
+
+	image206 = gtk_image_new_from_stock("gtk-goto-bottom", GTK_ICON_SIZE_MENU);
+	gtk_widget_show(image206);
+	gtk_image_menu_item_set_image(GTK_IMAGE_MENU_ITEM(edit_downolad), image206);
+
+	g_signal_connect((gpointer)fview_rename, "activate",
+			 G_CALLBACK(cb_fview_rename_activate),
+			 NULL);
+	g_signal_connect((gpointer)fview_delete, "activate",
+			 G_CALLBACK(cb_fview_delete_activate),
+			 NULL);
+	g_signal_connect((gpointer)edit_upload, "activate",
+			 G_CALLBACK(cb_fview_upload_activate),
+			 NULL);
+	g_signal_connect((gpointer)edit_downolad, "activate",
+			 G_CALLBACK(cb_fview_downolad_activate),
+			 NULL);
+
+	/* Store pointers to all widgets, for use by lookup_widget(). */
+	GLADE_HOOKUP_OBJECT_NO_REF(popup_menu_fview, popup_menu_fview, "popup_menu_fview");
+	GLADE_HOOKUP_OBJECT(popup_menu_fview, popup_separator, "popup_separator");
+	GLADE_HOOKUP_OBJECT(popup_menu_fview, fview_rename, "fview_rename");
+	GLADE_HOOKUP_OBJECT(popup_menu_fview, image203, "image203");
+	GLADE_HOOKUP_OBJECT(popup_menu_fview, fview_delete, "fview_delete");
+	GLADE_HOOKUP_OBJECT(popup_menu_fview, image204, "image204");
+	GLADE_HOOKUP_OBJECT(popup_menu_fview, edit_separator_one, "edit_separator_one");
+	GLADE_HOOKUP_OBJECT(popup_menu_fview, edit_upload, "edit_upload");
+	GLADE_HOOKUP_OBJECT(popup_menu_fview, image205, "image205");
+	GLADE_HOOKUP_OBJECT(popup_menu_fview, edit_downolad, "edit_downolad");
+	GLADE_HOOKUP_OBJECT(popup_menu_fview, image206, "image206");
+	GLADE_HOOKUP_OBJECT_NO_REF(popup_menu_fview, tooltips, "tooltips");
+
+	return popup_menu_fview;
+}
