f22bbeaf7e5ed15797c70e380fd0edd4f8ac29a5
niutao0602 <niutao0602@18169ff8-9de4-11dd-b47d-f93aca0b5c28>
2008-11-29 05:13:23 +0000

    在hostclient端添加Makefile文件

--- hostclient/src/main.c
+++ hostclient/src/main.c
@@ -20,10 +20,10 @@ int main(int argc, char *argv[])
 
 	gtk_set_locale();
 	gtk_init(&argc, &argv);
-
-	//add_pixmap_directory(PACKAGE_DATA_DIR "/" PACKAGE "/pixmaps");
+	add_pixmap_directory("/usr/share/linuxarms-hostclient/pixmaps");
 	add_pixmap_directory("./pixmaps");
 	add_pixmap_directory("../pixmaps");
+	add_pixmap_directory("/usr/share/linuxarms-hostclient/doc");
 	add_pixmap_directory("./doc");
 	add_pixmap_directory("../doc");
 	window_main = create_window_main();
