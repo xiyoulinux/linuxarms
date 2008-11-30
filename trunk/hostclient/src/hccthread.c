4e7976fb98c20bd52a1a4b0578107df6f2d6f15f
chengov <chengov@18169ff8-9de4-11dd-b47d-f93aca0b5c28>
2008-11-30 13:37:42 +0000

    修改

--- trunk/hostclient/src/hccthread.c
+++ trunk/hostclient/src/hccthread.c
@@ -10,38 +10,45 @@ boolean hcthread_init(struct hcthread_struct *hcthread,
 		struct hnet_struct *socket,
 		struct hcthread_trans *trans)
 {
+	return TRUE;
 }
 /*
  * 发送一个请求到armserver上
  */
 boolean hcthread_send(struct hcthread_struct *hcthread)
 {
+	return TRUE;
 }
 /*
  *接受一个数据从armserver
  */
 boolean hcthread_recv(struct hcthread_struct *hcthread)
 {
+	return TRUE;
 }
 
-/*
- * 实时控制主线程执行体
- * @p:  struct hcthread_struct
- */
 boolean showinfo(struct hcthread_struct *hcthread)
 {
 /* 显示信息 */
+	return TRUE;
 }
+/*
+ *实时控制主线程执行体
+ *@p:  struct hcthread_struct
+ */ 
 gboolean hcthread_thread(void *p)
 {
 	struct hcthread_struct *hcthread = (struct hcthread_struct *)p;
 	/* 建立网络连接 */
 	while (TRUE) {
-		/* 接收数据 */
-		/* 分析处理数据 */
-		/* 如果未接受到协议CSENDALL ，则循环接受，同时向arm端发送协议
-		 * CRECV */
-		/* 如果接受到协议CRECVALL，则退出 */
+		do {
+			hcthread_recv(hcthread);        /* 接收从arm端输出过来的数据 */  
+			if(hcthread->trans.protocol == CSEND) {
+				hcthread_send(hcthread);/* 发送协议CRECV */
+				showinfo(hcthread);     /* 显示所接受的数据 */
+			}
+		} while (hcthread->trans.protocol != CSENDALL);
+		hcthread_send(hcthread);                 /* 发送协议CRECVALL */
 	}
 	return TRUE;
 }
