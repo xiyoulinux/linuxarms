6215a79dc12f8cf401302c78b91012f91c7aa411
lizeliang.linux <lizeliang.linux@18169ff8-9de4-11dd-b47d-f93aca0b5c28>
2008-12-04 09:55:24 +0000

    初步完成了客户端登录界面图形处理的相关处理。

--- trunk/hostclient/src/clogin.c
+++ trunk/hostclient/src/clogin.c
@@ -7,28 +7,90 @@
 #include "debug.h"
 #include "linuxarms.h"
 
+struct user_struct user;
 /*
  * callback functions
  */
 void cb_login_rem_toggled(GtkToggleButton *togglebutton, gpointer user_data)
 {
-	debug_where();
+	if (gtk_toggle_button_get_active(GTK_TOGGLE_BUTTON(user_data)) == TRUE) {
+		user.is_record = 1;
+#if 1
+		printf("is_recode = 1 \n");
+
+	} else {
+		printf("is_recorde = 0 \n");	
+#endif
+	}
 }
 
 
 void cb_login_help_clicked(GtkButton *button, gpointer user_data)
 {
-	debug_where();
+	//debug_where();
+	GtkWidget *dialog;
+	const gchar *license =
+		"This library is free software; you can redistribute it and/or\n"
+		"modify it under the terms of the GNU Library \n"
+		"General Public License as published by the \n"
+		"Free Software Foundation; either version 2 of the License, \n"
+		"or (at your option) any later version.\n"
+		"This library is distributed in the hope that it will be useful,\n"
+		"but WITHOUT ANY WARRANTY; without even the implied \n"
+		"warranty of MERCHANTABILITY or \n"
+		"FITNESS FOR A PARTICULAR PURPOSE.\n"
+		"See the GNU Library General Public License for more details.\n"
+		"\n"
+		"You should have received a copy of the GNU Library General Public\n"
+		"License along with the Gnome Library; see the file COPYING.LIB.  \n"
+		"If not,write to the Free Software Foundation, Inc., \n"
+		"59 Temple Place - Suite 330,Boston, MA 02111-1307, USA.\n";
+
+	dialog = gtk_message_dialog_new (GTK_WIDGET(user_data),
+                                  GTK_DIALOG_DESTROY_WITH_PARENT,
+                                  GTK_MESSAGE_QUESTION,
+                                  GTK_BUTTONS_CLOSE,
+                                  "%s",
+                                  license);
+	gtk_widget_show(dialog);
+ /* Destroy the dialog when the user responds to it (e.g. clicks a button) */
+ 	g_signal_connect_swapped (dialog, "response",
+                           G_CALLBACK (gtk_widget_destroy),
+                           dialog);  
 
 }
 
 
 void cb_login_ok_clicked(GtkButton *button, gpointer user_data)
 {
-	debug_where();
-
+	//debug_where();
+	
 }
 
+void cb_comboboxentry_ip_changed(GtkComboBox *widget, gpointer user_data)
+{
+	char buf[20];
+	char *point;
+	GtkTreeIter iter;
+	point = gtk_combo_box_get_active_text(widget);
+	//if (gtk_combo_box_get_focus_on_click(widget) == TRUE)
+	//	printf("mouse is move off\n");
+	//printf("%s\n",point);
+	//if (gtk_combo_box_get_active_iter(widget, &iter) == FALSE) 
+	//	printf("%s\n",point);
+	//gtk_combo_box_append_text(widget, point);
+	switch (gtk_combo_box_get_active(widget)) {
+	case 0:
+		printf("chioce 0\n");
+		break;
+	case 1:
+		printf("chioce 1\n");
+		break;
+	default:
+		break;
+	}
+	
+}
 
 /*
  * some handling functions for login using
