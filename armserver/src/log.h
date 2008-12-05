1ecd39cc51f0af138ceefe70ced317446ce14903
chengov <chengov@18169ff8-9de4-11dd-b47d-f93aca0b5c28>
2008-12-05 12:33:20 +0000

    elementary finish --此行及以下内容将会被忽y--
    M    armserver/src/log.c
    M    armserver/src/log.h

--- armserver/src/log.h
+++ armserver/src/log.h
@@ -1,20 +1,21 @@
 #ifndef _LOG_H
 #define _LOG_H
+#include "linuxarms.h"
 
 #define USE_NAME_LEN 20
 #define DIR_LEN 30	
 #define FILE_SIZE 2
+#define LOGHOME_DIR "/root/linuxarms-armserver"
+
 struct log_struct {
-	int lock;
+	boolean lock;
 	char dir[DIR_LEN];
 	char usename[USE_NAME_LEN];
-	int fd;
+	FILE *fp;
 	int file_size;
-	void (*down_log_lock)(struct log_struct *); 
-	void (*up_log_lock)(struct log_struct *);
 };
 
 boolean armserver_init_log(char *usename);
 boolean write_log(char *log_data); 
-boolean clock_log();
+boolean close_log(struct log_struct *log);
 #endif
