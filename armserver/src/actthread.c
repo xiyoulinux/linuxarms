efe976ab29fd17eec8bb091aad7715487154059a
xuanfeng1104 <xuanfeng1104@18169ff8-9de4-11dd-b47d-f93aca0b5c28>
2008-12-11 13:20:36 +0000

    修改 actthread.c atthread.h

---
+++ armserver/src/actthread.c
@@ -0,0 +1,140 @@
+#include <string.h>
+
+#include "linuxarms.h"
+#include "protocol.h"
+#include "anet.h"
+#include "login.h"
+#include "atthread.h"
+#include "error.h"
+
+ static boolean atthread_get_download_file(struct atthread_struct *atthread, char *path);
+ static boolean atthread_get_upload_file(struct atthread_struct *atthread, char *path);
+ static boolean atthread_upload(struct atthread_struct *atthread, struct atthread_trans *trans);
+ static boolean atthread_download(struct atthread_struct *atthread, struct atthread_trans *trans);
+ static boolean atthread_send(struct atthread_struct *atthread);
+ static boolean atthread_recv(struct atthread_struct *atthread);
+ 
+ /*
+ * init_atthread	initialize atthread_struct structure. 
+ * @atthread:	the structure which will be initialized.
+ * @user:	the user who use armserve.
+ * @scoket:		tcp socket structure
+ */
+int atthread_init(struct atthread_struct *atthread,
+		     struct user_struct *user,
+		     struct anet_struct *socket,)
+{
+	int ret = 0;
+	if (!atthread) {
+		ret = ENOINIT;
+		goto out;
+	}
+	if (!user) {
+		ret = ENOINIT;
+		goto out;
+	}
+	if (!socket) {
+		ret = ENOSOCKET;
+		goto out;
+	}
+	atthread->select = FMAX;
+	atthread->socket = *socket;
+	atthread->user = *user;
+	atthread->trans.protocol = FMAX;
+
+	atthread->get_download_file = atthread_get_download_file;
+	atthread->get_upload_file = atthread_get_upload_file;
+	atthread->upload = atthread_upload;
+	atthread->download = atthread_download;
+	atthread->send = atthread_send;
+	atthread->recv = atthread_recv;
+	
+out:
+	return ret;
+}
+static boolean atthread_get_download_file(struct atthread_struct *atthread,
+								char *path)
+{
+	if (!atthread || !path)
+		return FALSE;
+	if (atthread->select == FDOWN) {
+		strcpy(atthread->upload_path, path);
+		return TRUE;
+	} else 
+		return FALSE;
+}
+
+static boolean atthread_get_upload_file(struct atthread_struct *atthread,
+								char *path)
+{
+	if (!atthread || !path)
+		return FALSE;
+	if (atthread->select == FUP) {
+		strcpy(atthread->download_path, path);
+		return TRUE;
+	} else 
+		return FALSE;
+}
+
+static boolean atthread_upload(struct atthread_struct *atthread, 
+					struct atthread_trans *trans)
+{
+	if (!atthread || !trans)
+		return FALSE;
+	return create_tcp_server(&atthread->socket);		
+}
+
+static boolean htthread_download(struct atthread_struct *atthread, 
+					struct atthread_trans *trans) {
+	if (!atthread || !trans)
+		return FALSE;
+	return create_tcp_server(&atthread->socket);
+}
+
+static boolean htthread_send(struct atthread_struct *atthread) {
+	if (!atthread)
+		return FALSE;
+	return anet_send(atthread->socket.tcp, &atthread->trans, 
+				sizeof(struct atthread_trans));
+
+}
+
+static boolean htthread_recv(struct atthread_struct *atthread)
+{
+	if (!atthread)
+		return FALSE;
+	return anet_recv(atthread->socket.tcp, &atthread->trans, 
+				sizeof(struct atthread_trans));
+
+}
+
+gboolean atthread_thread(void *p)
+{
+	struct atthread_struct *atthread = (struct atthread_struct *)p;
+	if (!create_tcp_server(&atthread->socket)) {
+		print_error(ESYSERR,"建立文件传输网络连接错误");
+		return FALSE;
+	}
+	while (TRUE) {
+		switch(atthread->select) {
+			case FUP: {
+				if(atthread->trans.protocol == FUP)
+				anet_send(atthread->socket.tcp, &atthread->trans,
+							sizeof(struct atthread_trans));
+				else
+					return FALSE;
+				break;
+			}
+			case FDOWN: {
+				if(atthread->trans.protocol == FDOWN)
+				anet_recv(atthread->socket.tcp, &atthread->trans,
+							sizeof(struct atthread_trans));
+				else 
+					return FALSE;
+				break;
+			}
+			default:return FALSE;
+		}
+	}
+	return TRUE;
+}
---
+++ armserver/src/atthread.h
@@ -0,0 +1,57 @@
+#ifndef _ATTHREAD_H
+#define _ATTHREAD_H
+
+#include "protocol.h"
+#include "anet.h"
+#include "login.h"
+#include "linuxarms.h"
+
+#define ATTHREAD_PATH_LEN 80
+#define TRAS_SIZE 512
+
+/*
+ * @atthread_trans     文件传输的数据
+ * @trans:		    控制协议
+ * @buffer:		  传输的数据
+ */
+struct atthread_trans {
+	protocol_fthread protocol;
+	char buffer[TRAS_SIZE];
+};
+
+/*
+ * @file_size:       请求传输文件的大小
+ * @savefile_size:   已经传输文件的大小
+ */
+struct atthread_struct {
+	protocol_fthread select;
+	long long file_size;
+	long long savefile_size;
+	struct user_struct user;
+	struct anet_struct socket;
+	struct atthread_trans trans;
+	char download_path[ATTHREAD_PATH_LEN];
+	char upload_path[ATTHREAD_PATH_LEN];
+
+	boolean (*get_download_file)(struct atthread_struct *atthread,
+							char *path);
+	boolean(*get_upload_file)(struct atthread_struct *atthread,
+							char *path);
+	boolean(*upload)(struct atthread_struct *atthread, 
+				struct atthread_trans *trans);
+	boolean(*download)(struct atthread_struct *atthread, 
+				struct atthread_trans *trans);
+	boolean (*send)(struct atthread_struct *atthread);
+	boolean (*recv)(struct atthread_struct *atthread);
+};
+
+/* 初始化htthread_struct结构体 */
+int atthread_init(struct atthread_struct *atthread,
+	             struct user_struct *user,
+		     struct anet_struct *socket);
+
+/* 线程执行体 */
+gboolean atthread_thread(void *p);
+
+#endif
+
