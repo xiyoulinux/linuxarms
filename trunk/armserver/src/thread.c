3d7a749d947beaaaec448194a5fdca2ec6b196d6
niutao0602 <niutao0602@18169ff8-9de4-11dd-b47d-f93aca0b5c28>
2009-02-23 12:30:11 +0000

    添加thread.h thread.c

---
+++ trunk/armserver/src/thread.c
@@ -0,0 +1,116 @@
+#include "linuxarms.h"
+#include "thread.h"
+#include "error.h"
+
+#define LINUXARMS_THREAD_POINTER(thread) do {              \
+	if (!(thread)) {                                   \
+		print_error(EWARNING, "invalid parameter");\
+		return;                                    \
+	}                                                  \
+} while (0)
+
+void linuxarms_thread_init(struct linuxarms_thread *thread)
+{
+	LINUXARMS_THREAD_POINTER(thread);
+	thread->state = TRUE;
+	thread->lock = linuxarms_thread_lock_create();
+	thread->id = NULL;
+}
+
+#ifdef GTK_THREAD
+
+linuxarms_thread_t *linuxarms_thread_create(THREADFUNC func, void *arg)
+{
+	return g_thread_create((void *)func, arg, TRUE, NULL);
+}
+
+linuxarms_thread_t *linuxarms_thread_self()
+{
+	return g_thread_self();
+}
+void linuxarms_thread_exit(linuxarms_thread_t **thread)
+{
+	//g_thread_join(thread);
+	*thread = NULL;
+}
+
+linuxarms_thread_lock_t *linuxarms_thread_lock_create(void)
+{
+	return g_mutex_new(); 
+}
+void linuxarms_thread_lock(struct linuxarms_thread *thread)
+{
+	LINUXARMS_THREAD_POINTER(thread);
+	thread->state = TRUE;
+	g_mutex_lock(thread->lock);
+}
+void linuxarms_thread_unlock(struct linuxarms_thread *thread)
+{
+	LINUXARMS_THREAD_POINTER(thread);
+	g_mutex_unlock(thread->lock);
+	thread->state = FALSE;
+}
+boolean linuxarms_thread_trylock(struct linuxarms_thread *thread)
+{
+	if (!thread)
+		return TRUE;
+	return thread->state;
+}
+void linuxarms_thread_lock_free(struct linuxarms_thread *thread)
+{
+	LINUXARMS_THREAD_POINTER(thread);
+	thread->state = TRUE;
+	g_mutex_free(thread->lock);
+}
+#else
+static void *pthread_ret;
+linuxarms_thread_t *linuxarms_thread_create(THREADFUNC func, void *arg)
+{
+	pthread_t *pthread;
+	pthread = (pthread *)malloc(sizeof(pthread_t));
+	if (pthread_create(pthread, NULL, func, arg) == -1) {
+		return NULL;
+	}
+	return pthread;
+}
+linuxarms_thread_t *linuxarms_thread_self()
+{
+	return &pthread_self();
+}
+void linuxarms_thread_exit(linuxarms_thread_t **thread)
+{
+	//pthread_join(thread, &pthread_ret);
+	free(*thread);
+	*thread = NULL;
+}
+linuxarms_thread_lock_t *linuxarms_thread_lock_create(void)
+{
+	pthread_mutex_t *lock;
+	lock = (pthread_mutex_t *)malloc(sizeof(pthread_mutex_t));
+	pthread_mutex_init(lock, NULL);
+	return lock;
+}
+void linuxarms_thread_lock(struct linuxarms_thread *thread)
+{
+	LINUXARMS_THREAD_POINTER(thread);
+	thread->state = TRUE;
+	pthread_mutex_lock(thread->lock);
+}
+void linuxarms_thread_unlock(struct linuxarms_thread *thread)
+{
+	LINUXARMS_THREAD_POINTER(thread);
+	pthread_mutex_unlock(thread->lock);
+	thread->state = FALSE;
+}
+boolean linuxarms_thread_trylock(struct linuxarms_thread *thread)
+{
+	if (!thread)
+		return TRUE;
+	return thread->state;
+}
+void linuxarms_thread_lock_free(linuxarms_thread_lock_t *lock)
+{
+	LINUXARMS_THREAD_POINTER(thread);
+	free(thread->lock);
+}
+#endif
