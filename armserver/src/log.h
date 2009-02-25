f467e40f5ddc56a74be8a0cea546e57fbb4c8d1e
chengov <chengov@18169ff8-9de4-11dd-b47d-f93aca0b5c28>
2009-02-25 10:53:21 +0000

    日志功能模块初步测试成功

--- armserver/src/log.h
+++ armserver/src/log.h
@@ -1,21 +1,23 @@
 #ifndef _LOG_H
 #define _LOG_H
 #include "linuxarms.h"
+#include "thread.h"
+#include "stdio.h"
 
+#define LOG_DIR_LEN 128	
 #define USE_NAME_LEN 20
-#define DIR_LEN 30	
 #define FILE_SIZE 2
 #define LOGHOME_DIR "/root/linuxarms-armserver"
 
 struct log_struct {
-	boolean lock;
-	char dir[DIR_LEN];
-	char usename[USE_NAME_LEN];
-	FILE *fp;
-	int file_size;
+	struct linuxarms_thread log_thread;
+	FILE *log_fp;
+	int log_file_size;
+	boolean log_inited;
 };
 
-boolean armserver_init_log(char *usename);
+
+boolean armserver_init_log(const char *usename);
 boolean write_log(char *log_data); 
-boolean close_log(struct log_struct *log);
+boolean close_log(void);
 #endif
